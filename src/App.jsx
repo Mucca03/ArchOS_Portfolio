ac03988c0e585e229ee2650d6949216dec8e2037
Mucca03 <alejito4d@gmail.com>
2025-11-23 18:44:49 -0500

    implement a hoc and added terminal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import gsap from "gsap";
+import { Draggable } from "gsap/Draggable"
+
 import { Dock, Navbar, Welcome } from "#components"
-import React from "react"
+import { Terminal } from "#windows";
+
+gsap.registerPlugin(Draggable);
 
 const App = () => {
   return(
         <main>
             <Navbar />
             <Welcome />
-             <Dock />
+            <Dock />
+
+            <Terminal/>
         </main>
   )
 }
